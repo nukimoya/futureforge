3f493714e09fa6b4193ff4de5ed3cfc7997d4cbe
Samuel Adelakun <sayomikun123@gmail.com>
2025-07-09 08:47:52 -0700

    Koyeb Port Readjustment

--- index.js
+++ index.js
@@ -42,7 +42,7 @@ sequelize
   })
   .catch(err => console.log('Error syncing database:', err));
 
- const PORT = process.env.PORT || 5000;
+ const PORT = process.env.PORT || 8000;
   app.listen(PORT, () => {
     console.log(`Server is running on port ${PORT}`);
   });
