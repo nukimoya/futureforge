ac7ab1f391cd5d266fd16a0320fbeacd6672ba3e
Samuel Adelakun <sayomikun123@gmail.com>
2025-07-11 14:24:14 -0700

    need to learn to rollback man fr

--- controllers/userController.js
+++ controllers/userController.js
@@ -1,7 +1,22 @@
-const express = require('express');
-
-const current_user = async (req, res) => {
-    res.json({ user: req.user});
-}
+const formatUserResponse = (user) => ({
+    id: user.id,
+    username: user.name,
+    email: user.email,
+    role: user.role,
+    takentest: user.hasTakenAssessment,
+  });
+  
+  const current_user = async (req, res) => {
+    const token = req.token || req.headers.authorization?.split(" ")[1];
+    return res.status(200).json({
+      data: {
+        user: formatUserResponse(req.user),
+        token,
+      },
+      requiresVerification: false,
+      msg: 'User data refreshed successfully',
+    });
+  };
+  
 
 module.exports = current_user;
