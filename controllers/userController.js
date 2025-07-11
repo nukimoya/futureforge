552f9b6f4525a96360efd697604d412eeb59bd94
Samuel Adelakun <sayomikun123@gmail.com>
2025-07-11 13:34:49 -0700

    Adjusted token collection and decoding again

--- controllers/userController.js
+++ controllers/userController.js
@@ -1,7 +1,7 @@
 const express = require('express');
 
 const current_user = async (req, res) => {
-    res.json({ user: req.user });
+    res.json({ user: req.user, token: req.token });
 }
 
 module.exports = current_user;
